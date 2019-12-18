a800942fa72fc31ac8dd49dcdde40d923de75939
jiiyiu <55100617+jiiyiu@users.noreply.github.com>
2019-12-18 23:33:36 +0100

    up
    tous les fichiers

---
+++ animalerie/animalerie.kt
@@ -0,0 +1,82 @@
+
+fun main() {
+
+    println("Bienvenue dans cet animalerie")
+
+    val profil = Animalerie()
+
+
+
+    var name = Animalerie()
+    val Livi: Animal = Chat()
+    val ErBao: Animal = Chien()
+    val Pikajou: Animal = Perroquet()
+
+
+    name.add(Livi)
+    name.add(ErBao)
+    name.add(Pikajou)
+
+
+    name.profil()
+
+}
+
+
+
+class Animalerie{
+
+    var Names = mutableListOf<Animal>()
+
+    fun add(animaux : Animal){
+        Names.add(animaux)
+    }
+
+    fun profil(){
+        println("Les animaux disponnibles sont: $Names")
+        for (animal in Names)
+        {
+            println("Type:")
+            animal.quiSuisJe()
+            println("Moyen de communication:")
+            animal.parler()
+
+        }
+    }
+}
+
+interface Animal{
+    fun quiSuisJe()
+    fun parler()
+
+}
+
+class Chien:Animal{
+    override fun quiSuisJe() {
+        println("chien")
+    }
+    override fun parler() {
+        println("aboieyement")
+    }
+
+}
+class Chat:Animal{
+    override fun quiSuisJe() {
+        println("chat")
+    }
+    override fun parler() {
+        println("miaulements")
+    }
+
+
+}
+class Perroquet:Animal{
+    override fun quiSuisJe() {
+        println("Perroquet")
+    }
+
+    override fun parler() {
+        println("imitation de la parole humaine")
+    }
+
+}
---
+++ plusoumoins/jeu.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+
+    var i = 1
+
+    println("Premier joueur, veuillez rentrer un nombre entre 1 et 10: ")
+    var mdp = readLine()
+
+    val compteur = null
+
+    while (mdp!= compteur) {
+
+        println("A ton tour joueur2, devine le nommbre entré par ton adversaire:")
+        var tenta = readLine()
+
+        if (tenta == mdp) {
+            if (i >2) {
+                println("Tu peux faire mieux")
+            } else if (i < 2) {
+                println("Bien joué!")
+            }
+            println("tu a reussi en $i essais")
+            println("Fin de partie")
+
+
+        } else if (mdp > tenta.toString()) {
+            print("C'est superieur\n")
+            i++
+        } else if (mdp < tenta.toString()) {
+            print("C'est inferieur\n")
+            i++
+        }
+    }
+    }
